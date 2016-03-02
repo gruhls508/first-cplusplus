2e6e6d284fa84a0c73fb8b6290e8727e007fa275
Glen Ruhl <intradinedev@gmail.com>
2016-03-02 17:27:08 -0500

    Initial Commit

---
+++ cplusplus_example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cplusplus_example
+//
+//  Created by Glen Ruhl on 3/2/16.
+//  Copyright © 2016 intradine. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
