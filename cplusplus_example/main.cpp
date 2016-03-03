dbc314ee70c2417fa4fafa5773d7e25a1dcfebf7
Glen Ruhl <intradinedev@gmail.com>
2016-03-03 12:44:10 -0500

    Used -stringstream() to get string input from user and pass to/convert other data types, in this case int and float

--- cplusplus_example/main.cpp
+++ cplusplus_example/main.cpp
@@ -7,16 +7,22 @@
 //
 
 #include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 
 int main(int argc, const char * argv[]) {
     string mystr;
-    cout << "What's your name? ";
-    getline (cin, mystr);
-    cout << "Hello " << mystr << ".\n";
-    cout << "What is your favorite team? ";
-    getline (cin, mystr);
-    cout << "I like " << mystr << " too!\n";
+    float price=0;
+    int quantity=0;
+
+    cout << "Enter price: ";
+    getline (cin,mystr);
+    stringstream(mystr) >> price;
+    cout << "Enter quantity: ";
+    getline (cin,mystr);
+    stringstream(mystr) >> quantity;
+    cout << "Total price: " << price*quantity << endl;
     return 0;
 }
